e1c3d180345278880b03115e3b06cd9ee7603899
matohawk <matohawk@gmail.com>
2013-05-30 08:07:20 -0400

    sol 4.1

--- GruntFile.js
+++ GruntFile.js
@@ -2,11 +2,19 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['js/lib/angular.min.js', 'js/app.js', 'js/controllers/mainController.js'],
+                dest: 'js/built.js'
+            }
+        }
     });
 
     // Load the plugin that provides the "concatenate" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
+    grunt.registerTask('default', ['concat:dist']);
 
 };
