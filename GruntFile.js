36efea2d9bb92a98106972180196a72719d97cb8
matohawk <matohawk@gmail.com>
2013-05-30 08:43:06 -0400

    add solution to the 4.2

--- GruntFile.js
+++ GruntFile.js
@@ -8,13 +8,22 @@ module.exports = function(grunt) {
                 src: ['js/lib/angular.min.js', 'js/app.js', 'js/controllers/mainController.js'],
                 dest: 'js/built.js'
             }
+        },
+        compass: {
+            dist: {
+                options: {
+                    cssDir: 'css',
+                    sassDir: 'css'
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "concatenate" task.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat:dist']);
+    grunt.registerTask('default', ['concat:dist', 'compass:dist']);
 
 };
